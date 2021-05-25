c99139e91fb24b40fa24c3e9c87b92cfad5b42ab
Michael Hrabe <michael.hrabe@gmail.com>
2021-05-25 22:19:45 +0200

    Adding router

---
+++ src/components/blog/Blog.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let name;
+  // import Nested from './Nested.svelte';
+  let htmlContent = ''
+fetch('/build/petshop_cfg.html').then(response => {
+  console.dir(response)
+  return response.text();
+}).then(html => {
+  htmlContent = html
+})
+</script>
+
+<main>
+  <div class="content">{@html htmlContent}</div>
+</main>
+
+<style>
+  main {
+      text-align: center;
+      padding: 1em;
+      max-width: 240px;
+      margin: 0 auto;
+  }
+
+  /* .content {
+      text-align: center;
+  } */
+</style>
